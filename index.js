677bb9b49d0350a38723fda23e32450450b77f3e
Daniel Vanpelt <70976643+dvanpelt-hub@users.noreply.github.com>
2020-10-06 15:59:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-function handleFormSubmit() {
-    //This function handles the submit button for user input
-  $("#dog-submit").submit((event) => {
-    event.preventDefault();
-    let dogValue = $("#dog-amount").val();
-    console.log(dogValue);
-    handleImageCall(dogValue);
-  });
-}
-
 function handleImageCall(userInput) {
     //This function handles the user input and returns results accordingly
     if (userInput >= 3 && userInput <= 50) {
@@ -18,27 +8,51 @@ function handleImageCall(userInput) {
         .then(responseJson => displayResults(responseJson))
         .then(responseJson => console.log(responseJson));
     }
-    else {
+    else if (userInput < 3 || userInput > 50) {
         //Uses a promise
         fetch(`https://dog.ceo/api/breeds/image/random/3`)
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
-        .catch(error => alert("Try again please"));
+          .then((response) => response.json())
+          .then((responseJson) => displayResults(responseJson))
+          .then((responseJson) => console.log(responseJson))
+          alert("Please select a number between 1 and 50 next time, but for now here are 3 dogs for you!");
+    }
+    else {
+        alert("Sorry, please enter an amount between 1 and 50");
     }
 };
 
 function displayResults(responseJson) {
+  let myMap = responseJson.message;
+  console.log("Here are the mapped results: " + myMap);
   console.log(responseJson);
-  $(".image-results").html("");  
-  responseJson.message.forEach(receivedImage => {
-      $(".results").append(`<img src="${receivedImage}" class="image-results">`
+  $(".results").html("");
+  responseJson.message.forEach(responseImg => {
+    $(".results").append(`<img src="${responseImg}" class="results">`);
+  });  
+  responseJson.message.forEach(myMap => {
+      $(".image-results").replaceWith(
+      `<img src="${myMap}" class="image-results">`
       );
   //display the results section
       $(".results").removeClass("hidden");
   }
 )};
 
+function handleFormSubmit() {
+  //This function handles the submit button for user input
+  $("#dog-submit").submit((event) => {
+    event.preventDefault();
+    let dogValue = $("#dog-amount").val();
+    console.log(dogValue);
+    handleImageCall(dogValue);
+  });
+}
+
 $(function() {
     console.log("test");
     handleFormSubmit();
 });
+
+/*use the array function map which returns a <img> tags with the src containing the url
+
+all these img tags should be wrapped in a parent div*/
