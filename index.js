ed38b3a9a7a8936bdcc8638e50d53437c72490e4
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-10-06 08:57:47 -0400

    final version 1.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function handleFormSubmit() {
+    //This function handles the submit button for user input
   $("#dog-submit").submit((event) => {
     event.preventDefault();
     let dogValue = $("#dog-amount").val();
@@ -10,18 +11,32 @@ function handleFormSubmit() {
 }
 
 function handleImageCall(userInput) {
+    //This function handles the user input and returns results accordingly
     if (userInput >= 3 && userInput <= 50) {
         fetch(`https://dog.ceo/api/breeds/image/random/${userInput}`)
         .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
         .then(responseJson => console.log(responseJson));
     }
     else {
+        //Uses a promise
         fetch(`https://dog.ceo/api/breeds/image/random/3`)
-        .then((response) => response.json())
-        .then((responseJson) => console.log(responseJson))
-        .catch((error) => alert("Try again please"));
+        .then(response => response.json())
+        .then(responseJson => console.log(responseJson))
+        .catch(error => alert("Try again please"));
     }
 };
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $(".image-results").html("");  
+  responseJson.message.forEach(receivedImage => {
+      $(".results").append(`<img src="${receivedImage}" class="image-results">`
+      );
+  //display the results section
+      $(".results").removeClass("hidden");
+  }
+)};
 
 $(function() {
     console.log("test");
